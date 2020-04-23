e3a019c5a9b8df4a34e634e15d713d4fa9abcdcd
sajid7053 <39021751+sajid7053@users.noreply.github.com>
2020-04-23 22:23:14 +0530

    Update ApiKeyController.php

--- src/Controller/ApiKeyController.php
+++ src/Controller/ApiKeyController.php
@@ -6,12 +6,12 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ApiKeyController extends ControllerBase {
-	public function ShowKey() {
+  public function ShowKey() {
     $config =\Drupal::config('node_json_data.apikey');
     $key = $config->get('Api_key');
 	  return [
-      '#type' => 'markup',
-      '#markup' => $this->t('@key',['@key'=>$key])
-    ];
+            '#type' => 'markup',
+            '#markup' => $this->t('@key',['@key'=>$key])
+          ];
   }
 }
